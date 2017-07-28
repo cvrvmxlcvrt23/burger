5ffdd911b0c29ee22d577d283f8465d675236f14
curtis ballard <curtisballard@curtiss-MacBook-Air.local>
2017-07-27 22:54:42 -0700

    burgers

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+-- Seed Burgers into Database ... No Sesame! (hehehe)
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Big Mac", false, "2016-11-05 04:20:00"),
+  ("Whooper", false, "2016-11-05 04:20:01"),
+  ("McGangBang", false, "2016-11-05 04:20:02"),
+  ("Double Bacon Cheese burger", false, "2016-11-05 16:20:00"),
+  ("Quarter Pounder", false, "2016-11-05 16:20:01"),
+  ("McRoyal", false, "2016-11-05 04:20:20"),
+  ("CoolinCuhzCheeseBurger", true, "2016-11-05 16:20:16");
